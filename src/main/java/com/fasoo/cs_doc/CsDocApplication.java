9a8836de1f3da74248308ea1cf7bc91cce8ae878
jhcha0822 <jhcha0822@gmail.com>
2026-01-18 23:00:01 +0900

    chore: initial project setup

---
+++ src/main/java/com/fasoo/cs_doc/CsDocApplication.java
@@ -0,0 +1,13 @@
+package com.fasoo.cs_doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsDocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fasoo/cs_doc/CsDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fasoo.cs_doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
